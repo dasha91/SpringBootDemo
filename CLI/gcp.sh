7529ae55d672fd6210fa41220607668a880f591f
Danial Shakeri <dasha@microsoft.com>
2023-07-12 10:45:13 -0700

    got the application working locally with postgres local

--- CLI/gcp.sh
+++ CLI/gcp.sh
@@ -3,30 +3,33 @@
 gcloud components install docker-credential-gcr
 gcloud components install kubectl
 gcloud components install gke-gcloud-auth-plugin
+gcloud services enable sqladmin.googleapis.com
+# Enable the following APIs: Artifact Registry, Kubernetes Engine API
 
 ##################################################  Set Parameters ##################################################
-gcloud config set project angular-axe-306514
-gcloud config set compute/zone us-west1
+gcloud config set project cci-sandbox-danial
 
-Create artifact repository
+##################################################  Create Artifact registry ##################################################
 gcloud artifacts repositories create spring-boot-demo-repo --repository-format=docker --location=us-west1
 
-docker buildx build --platform=linux/amd64  -t us-west1-docker.pkg.dev/angular-axe-306514/spring-boot-demo-repo/spring-boot-demo:v9 .
+################################################## Build docker image ##################################################
 
-gcloud artifacts repositories add-iam-policy-binding spring-boot-demo-repo \
-    --location=us-west1 \
-    --member=serviceAccount:204515338375-compute@developer.gserviceaccount.com \
-    --role="roles/artifactregistry.reader"
+#If you're on a M1 mac: 
+docker buildx build --platform=linux/amd64 -t us-west1-docker.pkg.dev/cci-sandbox-danial/spring-boot-demo-repo/spring-boot-demo:v1 .
 
+################################################## push docker image to artifact registry ##################################################
 gcloud auth configure-docker us-west1-docker.pkg.dev
+docker push us-west1-docker.pkg.dev/cci-sandbox-danial/spring-boot-demo-repo/spring-boot-demo:v1
 
-docker push us-west1-docker.pkg.dev/angular-axe-306514/spring-boot-demo-repo/spring-boot-demo:v9
-
-kubectl create deployment spring-boot-deployment9 --image=us-west1-docker.pkg.dev/angular-axe-306514/spring-boot-demo-repo/spring-boot-demo:v9
-
-docker tag spring-boot-demo.jar:latest us-west1-docker.pkg.dev/angular-axe-306514/spring-boot-demo-repo/spring-boot-demo:v9
-
+################################################## Create cluster  ##################################################
 gcloud container clusters create spring-boot-cluster
-
 gcloud container clusters get-credentials spring-boot-cluster
 
+################################################## Create deployment to  ##################################################
+kubectl create deployment spring-boot-deployment1 --image=us-west1-docker.pkg.dev/cci-sandbox-danial/spring-boot-demo-repo/spring-boot-demo:v1
+kubectl apply Load-balancer-gcp.yaml #Make sure to update internal-lb app to the deployment name: spring-boot-deployment1
+
+################### Create Postgres db ######################
+gcloud sql instances create spring-boot-postgres-instance --database-version=POSTGRES_14 --cpu=2 --memory=7680MB --region=us-west1
+gcloud sql users set-password postgres --instance=spring-boot-postgres-instance --password=Sup3rS3cret!
+gcloud sql databases create demo --instance=spring-boot-postgres-instance
--- src/main/java/com/cci/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/cci/SpringBootDemo/SpringBootDemoApplication.java
@@ -2,8 +2,9 @@ package com.cci.SpringBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-
+@EntityScan
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
